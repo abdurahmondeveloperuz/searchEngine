e5ae2a248c3c240ff2c856831ef1c34981595f9e
abdurahmondeveloperuz <154701401+abdurahmondeveloperuz@users.noreply.github.com>
2025-06-06 17:35:22 +0500

    Update search.py

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 from googlesearch import search
 
 def search_google(Query, num_results=10):
-    SearthResults = search(Query, num=num_results, start=0, stop=num_results)
+    SearthResults = search(Query, start=0, stop=num_results)
     
     return SearthResults
 
